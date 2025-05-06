0134cccff220bf3bb1c88861e32670888365eca8
Takacs Martin <originalmartin97@gmail.com>
2025-05-06 18:51:24 +0000

    fix: update bio in About section for accurate representation of interests

--- src/data/about.js
+++ src/data/about.js
@@ -1,6 +1,6 @@
 const about = {
     name: "Martin",
-    bio: "I am a master's student in Computer Science at Széchenyi István University, Győr, Hungary. Passionate about software engineering and creative tech.",
+    bio: "I am a master's student in Computer Science at Széchenyi István University, Győr, Hungary. Passionate about computer science and creative tech.",
     profilePic: "/images/profile.jpg" // store this image in /public/images
   };
   
