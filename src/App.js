71082b6eda3d297e2e5d3893a88299ed8b0d3873
Takacs Martin <originalmartin97@gmail.com>
2025-05-06 18:19:20 +0000

    fix: adjust top margin of Container in App component for improved layout

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <Container maxWidth="lg" sx={{ mt: 8 }}>
+      <Container maxWidth="lg" sx={{ mt: 0 }}>
         <Box 
           id="about" 
           sx={{ 
